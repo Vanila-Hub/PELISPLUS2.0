545a7847c75ff5d8b4718e301b73804948d24f2e
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2025-07-02 23:20:52 +0200

    Refactor CORS middleware configuration and update user routes import

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
-const usr_routs = require("./routes/user");
+const usr_routs = require("./routes/Enpoints");
 const home_routs = require("./routes/home");
 
 //const UserModel = require('/models/UserModel');
@@ -13,9 +13,30 @@ const port = 2007;
 
 /*middleware*/
 app.use(cors({
-  origin: "*"
+  origin: true, // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: [
+    "Content-Type", 
+    "Authorization", 
+    "X-Requested-With",
+    "Accept",
+    "Origin",
+    "Access-Control-Request-Method",
+    "Access-Control-Request-Headers"
+  ],
+  credentials: false,
+  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
 }));
 app.use(express.json());
+
+// Handle preflight requests explicitly
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+  res.sendStatus(200);
+});
+
 app.use("/pelis-plus", usr_routs, home_routs);
 
 /* LEVANTAMOS SERVIDOR */
